e7547c61cc0c463e9fe3560531cf43fee21bf430
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-02 17:53:15 +0530

    check

---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h3>Essential Links</h3>
+
+    <h3>Ecosystem</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4b4b4b;
+}
+</style>
