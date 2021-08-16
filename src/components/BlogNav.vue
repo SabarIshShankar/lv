7ae05680f80d0af4203855b30df7a9bd7abdf369
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-16 16:51:06 +0530

    update nav, post

--- src/components/BlogNav.vue
+++ src/components/BlogNav.vue
@@ -1,29 +1,46 @@
 <template>
-  <div id="navbar">
-    <div class="nav">
-      <input type="checkbox" id="nav-check" />
-      <div class="nav-header">
-        <div class="nav-title">CODER.</div>
-      </div>
-      <div class="nav-btn">
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
-
-      <div class="nav-links">
-        <a>Youtube</a>
-        <a>Discord</a>
-      </div>
-    </div>
-  </div>
+  <nav class="nav">
+    <h1 class="nav__title">
+      <router-link to="/">{{ content.title }}</router-link>
+    </h1>
+    <transition-group tag="menu" name="nav__item" class="nav__menu">
+      <li
+        v-for="label in labels"
+        class="nav__item"
+        :key="label"
+        @click="navBack"
+      >
+        <i class="nav__item--icon"></i>
+        <span class="nav__item--label">{{ label }}</span>
+      </li>
+    </transition-group>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "Navbar",
+  name: "blog-nav",
+  props: {
+    navs: Number,
+    content: Object,
+    filters: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    labels() {
+      return Object.keys(this.filters).map(
+        (filter) => this.content.labels[filter]
+      );
+    },
+  },
+  methods: {
+    navBack() {
+      if (this.navs && !this.filters.author) this.$router.go(-1);
+      else this.$router.push("/");
+    },
+  },
 };
 </script>
 
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,64 @@
+<template>
+  <transition name="post">
+    <article v-if="allReady" class="post">
+      <header class="post__header">
+        <h2 class="post_title">{{ title }}</h2>
+
+        <h3 class="post__meta">
+          by
+          <router-link class="psot__author" :to="`/by/${wordsl(author)}`">
+            {{ author }}
+          </router-link>
+          <span class="post__sep"></span>
+          <time>
+            {{ prettyDate(published) }}
+          </time>
+        </h3>
+
+        <blockquote class="post__subtitle">
+          {{ description }}
+        </blockquote>
+
+        <section class="post__body rte" v-html="content"></section>
+      </header>
+    </article>
+  </transition>
+</template>
+
+<script>
+import { wordsl, prettyDate } from "../helpers";
+
+export default {
+  name: "blog-post",
+  resource: "BlogPost",
+  props: { post: String },
+
+  data() {
+    return {
+      title: "",
+      author: "",
+      content: "",
+      published: "",
+      description: "",
+      ready: false,
+    };
+  },
+  computed: {
+    allReady() {
+      return this.ready && this.post;
+    },
+  },
+
+  methods: {
+    wordsl,
+    prettyDate,
+  },
+
+  mounted() {
+    if (!this.post) {
+      this.ready = true;
+      return;
+    }
+  },
+};
+</script>
