6d6757178cc24bc5e477574fb7c9774cbbabff78
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-17 12:03:25 +0530

    u2

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,49 +1,74 @@
 <template>
-<div class="lightbox" @click.self="closeLightbox">
-  <img :src="photoUrl(photo.filename)">
-  <div class="lightbox-info">
-    <div class="lightbox-info-inner">
-      <p v-if="photo.title">{{photo.title}}</p>
-      <p v-if="photo.location">{{photo.location}}</p>
-      <p v.if="photo.source"><a
-      rel="nofollow" :href="photo.source.url">{{photo.source.name}}
-        </a></p>
+  <div class="lightbox" @click.self="closeLightbox">
+    <img :src="photoUrl(photo.filename)" />
+    <div class="lightbox-info">
+      <div class="lightbox-info-inner">
+        <p v-if="photo.title">{{ photo.title }}</p>
+        <p v-if="photo.location">{{ photo.location }}</p>
+        <p v.if="photo.source">
+          <a rel="nofollow" :href="photo.source.url"
+            >{{ photo.source.name }}
+          </a>
+        </p>
+      </div>
     </div>
-
-  </div>
-  
   </div>
 </template>
 
 <script>
-import photos from '@/photos.json';
+import photos from "@/photos.json";
 
 export default {
   name: "Photo",
-  data(){
+  data() {
     return {
       photos,
-    }
-  }
+    };
+  },
+  computed: {
+    photo() {
+      return this.photos.find((photo) => {
+        return photo.id === Number(this.$route.params.id);
+      });
+    },
+  },
+  methods: {
+    photoUrl(filename) {
+      return require(`../assets/images/${filename}`);
+    },
+    closeLightbox() {
+      this.$router.push("/");
+    },
+  },
 };
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap");
 *,
 .title,
-h1 {
-  font-family: "Syncopate";
-  color: #fff;
-  right: 0;
-  position: relative;
-  padding-right: 20px;
+.lightbox {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
 }
-
-hr {
-  border-top: none;
-  border-right: none;
-  border-left: none;
-  border-bottom: 0.6px solid rgb(17, 17, 17);
+.lightbox img {
+  margin: auto;
+  width: 100%;
+  grid-column-start: 2;
+}
+.lightbox-info {
+  margin: auto 2rem auto 0;
+}
+.lightbox-info-inner {
+  background-color: #FFFFFF;
+  display: inline-block;
+  padding: 2rem;
 }
 </style>
 
